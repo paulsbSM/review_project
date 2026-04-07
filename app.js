73d3685c855757679c14b3dae25f0edac3f4a688
Paul <paul@smugmug.com>
2026-04-07 16:28:43 -0400

    add npm start script

---
+++ app.js
@@ -0,0 +1,4 @@
+import chalk from 'chalk';
+   console.log(chalk.green('Hello from Node.js!'));
+   console.log(chalk.blue('This text is blue.'));
+   console.log(chalk.red.bold('This text is red and bold.'));
