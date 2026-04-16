713c41b1bf9ce8d7d7a00720613fc80a03bba7b0
Paul <paul@smugmug.com>
2026-04-16 10:33:52 -0400

    add public html files

--- app.js
+++ app.js
@@ -1,15 +1,20 @@
 import express from 'express';
+   import { fileURLToPath } from 'url';
+   import path from 'path';
+
+   const __filename = fileURLToPath(import.meta.url);
+   const __dirname = path.dirname(__filename);
 
    const app = express();
 
    app.get('/', (req, res) => {
-     res.send('<h1>Hello from Express!</h1>');
+     res.sendFile(path.join(__dirname, 'public', 'index.html'));
+   });
+
+   app.get('/about', (req, res) => {
+     res.sendFile(path.join(__dirname, 'public', 'about.html'));
    });
 
    app.listen(3000, () => {
      console.log('Server running at http://localhost:3000');
-   });
-
-   app.get('/about', (req, res) => {
-     res.send('<h1>About Page</h1>');
    });
